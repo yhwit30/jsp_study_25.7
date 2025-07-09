b80ff2bdab9ed05b83059bcd085c3fd1c746493f
yhwit30 <yhwit30@gmail.com>
2025-07-09 20:51:25 +0900

    servlet 시작

---
+++ src/main/java/koreaIT/HomeMainServlet.java
@@ -0,0 +1,20 @@
+package koreaIT;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HomeMainServlet")
+public class HomeMainServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		response.getWriter().append("hello").append(request.getContextPath());
+	}
+
+}
