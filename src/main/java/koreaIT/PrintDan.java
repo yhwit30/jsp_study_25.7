606f9f4133bac1f6a08642313bdcea8e8fa20e4d
yhwit30 <yhwit30@gmail.com>
2025-07-09 21:27:55 +0900

    구구단 기초

--- src/main/java/koreaIT/PrintDan.java
+++ src/main/java/koreaIT/PrintDan.java
@@ -1,21 +1,29 @@
 package koreaIT;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/HomeMainServlet")
-public class HomeMainServlet extends HttpServlet {
+@WebServlet("/PrintDan")
+public class PrintDan extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
 		response.setContentType("text/html;charset=UTF-8");
-		response.getWriter().append("hello").append(request.getContextPath());
-		response.getWriter().append("<a href=\"http://localhost:8080/yhw_servlet/HomeMain2\">main2로 가기</a>");
+
+		response.getWriter().append("== 8단 ==<br>");
+
+		int dan = 8;
+
+		for (int i = 1; i < 10; i++) {
+			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, (dan * i)));
+
+		}
+
 	}
 
 }
