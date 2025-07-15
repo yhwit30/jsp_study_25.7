f43fd151b79534941a4e9f7a7208c951af857f61
yhwit30 <yhwit30@gmail.com>
2025-07-15 19:55:29 +0900

    jsp 활용

--- src/main/java/koreaIT/PrintDan.java
+++ src/main/java/koreaIT/PrintDan.java
@@ -1,6 +1,7 @@
 package koreaIT;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,38 +13,16 @@ public class PrintDan extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		request.getRequestDispatcher("/jsp/home/printDan.jsp").forward(request, response);
 
-		response.setContentType("text/html;charset=UTF-8");
-		
-		String inputDan = request.getParameter("dan");
-		String inputLimit = request.getParameter("limit");
-		String color = request.getParameter("color");
-		
-		System.out.println(inputDan);
-		System.out.println(inputLimit);
-		System.out.println(color);
-		
-
-		if (inputDan == null) {
-			inputDan = "2";
-		}
-		if (inputLimit == null) {
-			inputLimit = "1";
-		}
-		
-		
-		int dan = Integer.parseInt(inputDan);
-		int limit = Integer.parseInt(inputLimit);
-		
-		
-		response.getWriter().append(String.format("<style>div {color : %s;} h1{color : blue;}</style>", color));
-		response.getWriter().append(String.format("<h1>== %d 단 ==</h1>", dan));
-
-		for (int i = 1; i <= limit; i++) {
-			response.getWriter().append(String.format("<div>%d * %d = %d</div>", dan, i, (dan * i)));
-
-		}
 
 	}
 
 }
+
+
+
+
+
+
---
+++ src/main/webapp/jsp/home/printDan.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>구구단 출력</title>
+</head>
+<body>
+
+<h1>구구단 확인555</h1>
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius adipisci delectus quasi odit repellendus mollitia placeat dolorum consectetur voluptate doloremque ut numquam illum tenetur commodi repellat tempore velit obcaecati ipsum.</div>
+</body>
+</html>
