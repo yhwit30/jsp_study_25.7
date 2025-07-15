7df8ccb644361fbd5d88ebd102d5f12052044f2b
yhwit30 <yhwit30@gmail.com>
2025-07-15 19:35:10 +0900

    파라미터 색상 추가

--- src/main/java/koreaIT/PrintDan.java
+++ src/main/java/koreaIT/PrintDan.java
@@ -17,8 +17,11 @@ public class PrintDan extends HttpServlet {
 		
 		String inputDan = request.getParameter("dan");
 		String inputLimit = request.getParameter("limit");
+		String color = request.getParameter("color");
+		
 		System.out.println(inputDan);
 		System.out.println(inputLimit);
+		System.out.println(color);
 		
 
 		if (inputDan == null) {
@@ -32,11 +35,12 @@ public class PrintDan extends HttpServlet {
 		int dan = Integer.parseInt(inputDan);
 		int limit = Integer.parseInt(inputLimit);
 		
-		response.getWriter().append("== " + dan + "단 ==<br>");
-
+		
+		response.getWriter().append(String.format("<style>div {color : %s;} h1{color : blue;}</style>", color));
+		response.getWriter().append(String.format("<h1>== %d 단 ==</h1>", dan));
 
 		for (int i = 1; i <= limit; i++) {
-			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, (dan * i)));
+			response.getWriter().append(String.format("<div>%d * %d = %d</div>", dan, i, (dan * i)));
 
 		}
 
